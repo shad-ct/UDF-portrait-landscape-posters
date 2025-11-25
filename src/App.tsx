9829567b298da234f8603ea3f14980ce49f3ba73
Shad CT <muhammedshad9895@gmail.com>
2025-11-25 14:59:46 +0530

    changed name to election poaster maker

--- src/App.tsx
+++ src/App.tsx
@@ -233,7 +233,7 @@ function App() {
           }}
         >
           <h1 className="text-2xl font-bold text-center mb-4 text-gray-100">
-            Sweet of madeena
+            ELECTION POSTER MAKER
           </h1>
 
           {/* Ratio Selection */}
